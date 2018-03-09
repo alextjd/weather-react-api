478d4bc9feb68ffe2b580babae81d0ad8e80fb55
alextjd <alextjd1@gmail.com>
2018-03-09 20:11:42 +0100

    Fuck skeleton.

--- src/App/App.js
+++ src/App/App.js
@@ -1,19 +1,18 @@
 import React, {Component} from 'react';
 import './App.css';
 import Title from "../Title/Title";
+import Form from "../Form/Form";
 
 
 class App extends Component {
     render() {
         return (
-            <div className="App container">
-                <div className="row">
-                    <div className="left five columns">
-                        <Title/>
-                    </div>
-                    <div className="right seven columns">
-                        
-                    </div>
+            <div className="App">
+                <div className="left">
+                    <Title/>
+                </div>
+                <div className="right">
+                    <Form/>
                 </div>
             </div>
         );
