e9e37f4a5fb22c1e5fe427a3daa19b353fe27e75
Rafa Tejada <rafatejada@iMac-de-Rafa-Tejada.local>
2018-03-09 19:44:20 +0100

    Adding the rows and cols skeleton display.

--- src/App/App.js
+++ src/App/App.js
@@ -1,14 +1,23 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
+import Title from "../Title/Title";
+
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App container">
+                <div className="row">
+                    <div className="left five columns">
+                        <Title/>
+                    </div>
+                    <div className="right seven columns">
+                        
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
