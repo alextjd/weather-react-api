bea0904a197917342ebe4e7ccc8285f450ae109d
alextjd <alextjd1@gmail.com>
2018-03-10 18:04:27 +0100

    Import mobile.css.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import './mobile.css';
 import App from './App/App';
 import registerServiceWorker from './registerServiceWorker';
 
