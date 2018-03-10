6bf1a88cb2d2659dbe2804971b82b849733cf68f
alextjd <alextjd1@gmail.com>
2018-03-10 13:42:54 +0100

    Conditional rendering depending on the props.

--- src/Weather/Weather.js
+++ src/Weather/Weather.js
@@ -1,14 +1,20 @@
-import React, {Component} from 'react';
+import React from 'react';
 
 const Weather = (props) => {
-    return(
-        <div className="weather-container">
-            <div className="wdata"><span>Location</span></div>
-            <div className="wdata"><span>Temperature</span></div>
-            <div className="wdata"><span>Humidity</span></div>
-            <div className="wdata"><span>Conditions</span></div>
-        </div>
-    );
+    if (props.city && props.country) {
+        return (
+            <div className="weather-container">
+                <div className="wdata"><span>Location</span><h3>{props.city}, {props.country}</h3></div>
+                <div className="wdata"><span>Temperature</span><h3>{props.temperature} °C</h3></div>
+                <div className="wdata"><span>Humidity</span><h3>{props.humidity}%</h3></div>
+                <div className="wdata"><span>Conditions</span><h3>{props.conditions}</h3></div>
+            </div>
+        );
+    } else {
+        return (
+            <div className="weather-container"><span className="error">{props.error}</span></div>
+        );
+    }
 };
 
 export default Weather;
