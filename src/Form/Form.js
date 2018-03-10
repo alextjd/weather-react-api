feb29a3540b46d92456ca6c375b4d50f0bdf1053
alextjd <alextjd1@gmail.com>
2018-03-10 12:44:00 +0100

    Fixed the onsubmit function.

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -2,16 +2,16 @@ import React from 'react';
 
 const Form = (props) => {
     return (
-        <form className="form-container container">
+        <form className="form-container container" onSubmit={props.getWeather}>
             <div className="row">
                 <div className="four columns">
-                    <input type="text" placeholder="City.."/>
+                    <input type="text" placeholder="City.." name="city" required/>
                 </div>
                 <div className="four columns">
-                    <input type="text" placeholder="Country.."/>
+                    <input type="text" placeholder="Country.." name="country" required/>
                 </div>
                 <div className="four columns">
-                    <button onClick={props.getWeather()}>Get weather</button>
+                    <button>Get weather</button>
                 </div>
             </div>
         </form>
