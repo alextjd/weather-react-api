7eb838c4b2090f412b4d02421e357d88a1c2e7a7
alextjd <alextjd1@gmail.com>
2018-03-10 18:03:14 +0100

    Removed skeleton classes.

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -1,19 +1,12 @@
 import React from 'react';
+import './Form.css';
 
 const Form = (props) => {
     return (
-        <form className="form-container container" onSubmit={props.getWeather}>
-            <div className="row">
-                <div className="four columns">
-                    <input type="text" placeholder="City.." name="city" required/>
-                </div>
-                <div className="four columns">
-                    <input type="text" placeholder="Country.." name="country" required/>
-                </div>
-                <div className="four columns">
-                    <button>Get weather</button>
-                </div>
-            </div>
+        <form className="form-container" onSubmit={props.getWeather}>
+            <input type="text" placeholder="City.." name="city" required/>
+            <input type="text" placeholder="Country.." name="country" required/>
+            <button>Get weather</button>
         </form>
     );
 };
