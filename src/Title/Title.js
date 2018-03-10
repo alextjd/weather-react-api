f095c0208ed6792e41712fbed7c9d08089703998
alextjd <alextjd1@gmail.com>
2018-03-10 16:34:54 +0100

    Minor changes.

--- src/Title/Title.js
+++ src/Title/Title.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import './Title.css';
 
 const Title = () => {
     return(
         <div className="title-container">
-            <div className="title">What's the weather like?</div>
-            <div className="subtitle">A web application to check what's the weather like at the moment
+            <div className="title">React weather app</div>
+            <p className="subtitle">A web application to check what's the weather like at the moment
             in any city of the world. Just type the city name and its country and we will tell
-            you if you should take your umbrella with you today. </div>
-            <div className="subtitle">The app is based on React, although it uses an open weather API
-            and some asynchronous programming on the backend. Provided to you by a pale programmer.</div>
+            you if you should take your umbrella with you today. </p>
+            <p className="subtitle">The app is based on React, although it uses an open weather API
+            and some asynchronous programming on the backend. Provided to you by a pale programmer.</p>
         </div>
     );
 };
