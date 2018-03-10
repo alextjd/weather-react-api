bc4a17823d0fb8e56127ab3017bbf0229a699092
alextjd <alextjd1@gmail.com>
2018-03-10 12:39:44 +0100

    Getting the correct response when requesting to the API.

--- src/App/App.js
+++ src/App/App.js
@@ -4,12 +4,30 @@ import Title from "../Title/Title";
 import Form from "../Form/Form";
 import Weather from "../Weather/Weather";
 
+//API key of openweathermap
+const API_KEY = 'ced8fd01f5874b0d9113917e0f93dce6';
 
 class App extends Component {
 
-    handleGetWeather = (e) => {
+    //ECMAScript6, baby: arrow functions, new constructor, etc, etc.
 
-    }
+    //Get the weather of a specific city
+    handleGetWeather = async (e) => {
+        //Prevent the form from submiting the usual way
+        e.preventDefault();
+
+        const city = e.target.elements.city.value;
+        const country = e.target.elements.country.value;
+
+        //Do a request to the API with this info
+        //Request using Fetch with asynchronous programming (async/await)
+        const response = await fetch('http://api.openweathermap.org/data/2.5/weather?q='
+            + city + ',' + country + '&appid=' + API_KEY);
+
+        const data = await response.json();
+
+        console.log(data);
+    };
 
 
 
