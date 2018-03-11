600a2617c4c671cf39099365685a79f4a75682ed
alextjd <alextjd1@gmail.com>
2018-03-11 02:38:37 +0100

    Minor changes.

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -4,8 +4,8 @@ import './Form.css';
 const Form = (props) => {
     return (
         <form className="form-container" onSubmit={props.getWeather}>
-            <input type="text" placeholder="City.." name="city" required/>
-            <input type="text" placeholder="Country.." name="country" required/>
+            <input type="text" placeholder="City" name="city" required/>
+            <input type="text" placeholder="Country" name="country" required/>
             <button>Get weather</button>
         </form>
     );
